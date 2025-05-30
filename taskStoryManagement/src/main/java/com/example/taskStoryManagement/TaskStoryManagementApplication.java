6bdb78cb92375112c85e5e4de828540d63c3fa98
naman-1998 <namanpanchal17@gmail.com>
2025-05-30 14:09:33 +0530

    Initial Commit : Task & Story Management System

---
+++ taskStoryManagement/src/main/java/com/example/taskStoryManagement/TaskStoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskStoryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskStoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskStoryManagementApplication.class, args);
+	}
+
+}
---
+++ taskStoryManagement/src/test/java/com/example/taskStoryManagement/TaskStoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskStoryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskStoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
